0ebb673d7e906f5b88187e54c99f11ee7bfc905f
Penjuyu <s.pochemuchka@mail.ru>
2018-06-20 14:24:01 +0300

    flower

---
+++ Assets/Scripts/Flower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+    Animator anim;
+    bool isClose = false;
+	
+	void Start ()
+    {
+        anim = GetComponent<Animator>();	
+	}
+
+    private void OnMouseDown()
+    {
+        isClose = !isClose;
+        anim.SetBool("IsClose", isClose);
+    }
+
+
+
+
+}
